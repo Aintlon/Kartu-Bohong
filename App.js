bcb6bc7886c71f99dda4fc2dd3f16ba31a56efb2
Aintlon <aintlon4u@gmail.com>
2017-11-05 09:35:29 +0700

    Add All Files

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hehehe</Text>
+        <Text>hah?</Text>
+        <Text>hehehe</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
